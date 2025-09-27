00784a54f7cadf9d325faad3018c6e56ec55cdf6
Alfin <alfinnn831@gmail.com>
2025-09-27 18:43:25 +0700

    View & Controller
    Membuat Tampilan dan menghubungkan dengan menggunakan controller

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,16 @@
 import { render } from "ejs"
 import express from "express"
+import * as profileController from "../controller/profileController.js"
+import * as profileController1 from "../controller/profileController1.js"
 
 const web = express.Router()
 
-web.get('/:username/', (req, res) => {
-    const username = req.params.username;
-    res.render ('public-profile', {
-        title : username,
-        username : username,
-        bio : "Hiduplah Seperti Denis Adit Tolongin Ditt.."
-    })
+web.get('/', (req,res) => {
+    res.render('index')
 })
+
+web.get('/:username', profileController.publicProfile)
+
+web.get('/profile/:username', profileController1.publicProfile1)
 
 export default web
